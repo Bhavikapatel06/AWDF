199852b61ca47a89d26110457c449046690489c4
Patel Bhavika <bhavikapatel4298@gmail.com>
2026-07-11 21:01:27 +0530

    Initial Commit

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+
+function About(){
+    return(
+        <>
+            <div id='about'>
+                <div>
+                    <h2>About Me</h2>
+                    <p>I am a passionate developer with experience in creating modern web applications.</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default About
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer(){
+    return(
+        <>
+            <footer id='footer'>
+                <p>&copy; 2023 My Portfolio. All rights reserved.</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
